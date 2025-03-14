642ff76b6828443e9932e6851de49e05ddf15066
Harish-Pulapa-7036 <harishkarthik7036@gmail.com>
2025-03-14 10:09:03 +0530

    added weight and qty to orders

--- src/pages/OrderList.jsx
+++ src/pages/OrderList.jsx
@@ -28,8 +28,8 @@ const OrderCard = ({ order }) => {
                         {order.orders.map((item, index) => (
                             <React.Fragment key={index}>
                                 <ListItem>
-                                    <ListItemText primary={item.productName} />
-                                    <Typography variant="body2" fontWeight="bold">₹{item.actualPrice}</Typography>
+                                    <ListItemText primary={`${item.productName} (Qty:${item.quantity},Wt:${item.weight})`} />
+                                    <Typography variant="body2" fontWeight="bold">{`₹${item.actualPrice}`}</Typography>
                                 </ListItem>
                                 {index < order.orders.length - 1 && <Divider />}
                             </React.Fragment>
