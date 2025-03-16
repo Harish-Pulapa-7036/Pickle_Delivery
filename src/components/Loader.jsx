20d95f201aed7a1444fbd87c36873f5e06626160
Harish-Pulapa-7036 <harishkarthik7036@gmail.com>
2025-03-16 11:50:55 +0530

    added loader text

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 export default function Loader({ showLoader }) {
     if (!showLoader) return null; // Don't render anything if showLoader is false
@@ -9,18 +10,26 @@ export default function Loader({ showLoader }) {
         <Box
             sx={{
                 position: 'fixed',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                zIndex: 11000,
+                top: 0,
+                left: 0,
                 width: '100%',
                 height: '100%',
+                zIndex: 11000,
                 display: 'flex',
+                flexDirection: 'column',
                 alignItems: 'center',
                 justifyContent: 'center',
+                backgroundColor: 'rgba(255, 255, 255, 0.1)', // 40% transparent white background
+                backdropFilter: 'blur(5px)', // Subtle blur effect
             }}
         >
             <CircularProgress />
+            <Typography 
+                variant="h8" 
+                sx={{ marginTop: 2, color: '#333', fontWeight: 'bold' }}
+            >
+                ⏳ Please wait, this may take a moment...
+            </Typography>
         </Box>
     );
 }
